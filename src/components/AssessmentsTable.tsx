7983843e9b4a7404d49b39449fb4292d2b1736d3
P541M <videna.psalmeleazar@gmail.com>
2025-05-20 15:07:03 -0400

    fixed eslint errors and notes icon

--- src/components/AssessmentsTable.tsx
+++ src/components/AssessmentsTable.tsx
@@ -11,12 +11,7 @@ import {
 import { useAuth } from "../contexts/AuthContext";
 import RichTextEditor from "./RichTextEditor";
 import { getFromLocalStorage, setToLocalStorage } from "../utils/localStorage";
-import {
-  formatLocalDate,
-  formatLocalDateTime,
-  getDaysUntil,
-  isUpcoming,
-} from "../utils/dateUtils";
+import { formatLocalDateTime, getDaysUntil } from "../utils/dateUtils";
 import { Assessment } from "../types/assessment";
 
 interface AssessmentsTableProps {
@@ -1216,7 +1211,12 @@ const AssessmentsTable: React.FC<AssessmentsTableProps> = ({
                           viewBox="0 0 20 20"
                           fill="currentColor"
                         >
-                          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                          <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                          <path
+                            fillRule="evenodd"
+                            d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
+                            clipRule="evenodd"
+                          />
                         </svg>
                       </button>
                     </td>
