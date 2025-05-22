32c21055e84895e1c39fc19a60bee1d113dd060a
P541M <videna.psalmeleazar@gmail.com>
2025-05-22 13:50:14 -0400

    adjusted icon stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,7 +31,37 @@ function MyApp({ Component, pageProps }: AppProps) {
           />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/images/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/images/android-chrome-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href="/images/android-chrome-512x512.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <main className={`${outfit.variable} ${lexend.variable} font-body`}>
           <Component {...pageProps} />
