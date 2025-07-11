591473ef2f22534b65ce242a08977328c9b3cfb4
P541M <videna.psalmeleazar@gmail.com>
2025-07-11 16:38:53 -0400

    updated more links

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -96,18 +96,13 @@ const Header = ({ onLogout }: HeaderProps) => {
     <>
       <header
         className={`navbar transition-all duration-300 ${
-          scrolled
-            ? "shadow-md"
-            : ""
+          scrolled ? "shadow-md" : ""
         }`}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4">
             <div className="flex items-center">
-              <a
-                href="https://asetta-landing.vercel.app"
-                className="flex items-center group"
-              >
+              <a href="https://asetta.me" className="flex items-center group">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-8 w-8 text-light-button-primary dark:text-dark-button-primary transition-transform duration-300 group-hover:scale-110"
@@ -116,9 +111,7 @@ const Header = ({ onLogout }: HeaderProps) => {
                 >
                   <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
                 </svg>
-                <span className="logo ml-2">
-                  Asetta
-                </span>
+                <span className="logo ml-2">Asetta</span>
               </a>
             </div>
             <div className="flex items-center space-x-4">
